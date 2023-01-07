a55851d4faf6668056bf32003c84aedc8d8820f1
matthewkohn <kohney319m@gmail.com>
2023-01-07 10:08:41 -0700

    Add styled components to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import styled from "styled-components";
 
 
 function App() {
   return (
-    <div>
-      <h1>Focus Fuzz</h1>
-      <span>Drown Out Distractions</span>
-    </div>
+    <AppContainer>
+      <Header>
+        <Title>Focus Fuzz</Title>
+        <Tagline>Drown Out Distractions</Tagline>
+      </Header>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  height: 100vh;
+  background: #DDC;
+  color: #333;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const Header = styled.div`
+  padding: 30px 0;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+`;
+
+const Tagline = styled.span`
+  font-style: italic;
+  letter-spacing: 2px;
+`;
