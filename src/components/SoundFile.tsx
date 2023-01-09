f012faf7c2cb6a6b09152890f5b8c748a0d2aabc
stevewhitmore <s25w89@gmail.com>
2023-01-09 11:21:16 -0600

    Converted component to function

--- src/components/SoundFile.tsx
+++ src/components/SoundFile.tsx
@@ -1,27 +1,17 @@
-import React from 'react';
 import styled from "styled-components";
 import { SoundFileModel } from '../models/sound-file.model';
 
-type SoundFileProps = SoundFileModel;
-type SoundFileState = {}
 
-class SoundFile extends React.Component<SoundFileProps, SoundFileState> {
-    constructor(props: any) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <SoundControl id={this.props.name}>
-                <span className="material-symbols-outlined">
-                { this.props.icon }
-                </span>
-                <label>{this.props.name}</label>
-                <input type="range"></input>
-            </SoundControl>
-        );
-    }
+const SoundFile = ( soundFile: SoundFileModel ) => {
+    return (
+        <SoundControl id={soundFile.name}>
+            <span className="material-symbols-outlined">
+            { soundFile.icon }
+            </span>
+            <label>{soundFile.name}</label>
+            <input type="range"></input>
+        </SoundControl>
+    );
 }
 
 export default SoundFile;
